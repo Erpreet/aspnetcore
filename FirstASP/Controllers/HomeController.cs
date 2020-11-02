26d32bfc46fcff420f6c81fbbe7be8c430a9520d
Erpreet <preetkaurchd15@gmail.com>
2020-11-02 11:17:45 -0700

    Pushing class work

--- FirstASP/Controllers/HomeController.cs
+++ FirstASP/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace FirstASP.Controllers
 {
     public class HomeController : Controller
     {
+
+       
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -28,10 +30,42 @@ namespace FirstASP.Controllers
             return View();
         }
 
+        public IActionResult TestPage(string item)
+        {
+            if(!string.IsNullOrWhiteSpace(item))
+            {
+                AddedItems.Add(item);
+            }
+
+            ViewBag.Items = AddedItems;
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public static List<Person> People = new List<Person>();
+        public void CreatePerson(string firstName, string lastName)
+        {
+            People.Add(new Person()
+            {
+                FirstName = firstName.Trim(),
+                LastName = lastName.Trim()
+            });
+        }
+
+        public void DeletePersonByFirstName( string firstName)
+        {
+            People.Remove(GetPersonByFirstName(firstName));
+           
+        }
+
+        public Person GetPersonByFirstName(string firstName)
+        {
+            return People.Where(x => x.FirstName.Trim().ToUpper() == firstName.Trim().ToUpper()).SingleOrDefault();
         }
     }
 }
